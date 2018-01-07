08d6c1794c6962539185a69863967161aac88c9a
Jordon R <reese.jordon@gmail.com>
2018-01-07 15:49:27 -0600

    Add divs to profileDetails

--- client/src/components/ProfileDetail.js
+++ client/src/components/ProfileDetail.js
@@ -10,8 +10,50 @@ FirstName, LastName, Birthday, UserName, Email, ACAID, Password
 import React from "react";
 
 function UserProfile() {
+
+
+
+
   return (
-  
+    <div className="ProfileInfo">
+      <div className="ProfilePicture"
+        Img
+      </div>
+      <div className="ProfileDetail">
+        FirstName
+      </div>
+      <div className="ProfileDetail">
+        LastName
+      </div>
+      <div className="ProfileDetail">
+        Birthday
+      </div>
+      <div className="ProfileDetail">
+        PhoneNumber
+      </div>
+      <div className="ProfileDetail">
+        Address Line1
+      </div>
+      <div className="ProfileDetail">
+        Address Line2
+      </div>
+      <div className="ProfileDetail">
+        City
+      </div>
+      <div className="ProfileDetail">
+        Zip
+      </div>
+      <div className="ProfileDetail">
+        Aca id
+      </div>
+      <div className="ProfileDetail">
+        Attended
+      </div>
+    </div>
+
+
+
+
   )
 }
 
